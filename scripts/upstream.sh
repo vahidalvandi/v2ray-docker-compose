d7e87267e8965dcd654260c49d36a88b7aa64666
Milad Rahimi <info@miladrahimi.com>
2022-10-21 16:05:03 +0330

    update

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -3,10 +3,8 @@
 LINK=https://raw.githubusercontent.com/miladrahimi/v2ray-docker-compose/master
 NAME=v2ray-upstream-server
 
-mkdir -p $NAME && cd $NAME || exit
+mkdir -p $NAME/config && cd $NAME || exit
 curl -s "$LINK/$NAME/docker-compose.yml" --output docker-compose.yml
-
-mkdir -p config
 curl -s "$LINK/$NAME/config/config.json" --output config/config.json
 
 printf "%s" "Enter UPSTREAM-UUID (Leave empty to generate a random one): "
@@ -15,9 +13,7 @@ if [ -z "$UPSTREAM_UUID" ]; then UPSTREAM_UUID=$(cat /proc/sys/kernel/random/uui
 
 sed -ie "s/<UPSTREAM-UUID>/$UPSTREAM_UUID/g" config/config.json
 
-echo "The Docker-compose and configuration files are ready."
+printf "The Docker-compose and configuration files are ready."
 
-UPSTREAM_IP=$(curl ifconfig.io)
-
-echo "UPSTREAM-IP: $UPSTREAM_IP"
-echo "UPSTREAM-UUID: $UPSTREAM_UUID"
+printf "UPSTREAM-IP: %s" "$(curl -s ifconfig.io)"
+printf "UPSTREAM-UUID: %s" "$UPSTREAM_UUID"
