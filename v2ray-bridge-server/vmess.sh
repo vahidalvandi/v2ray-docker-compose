0ff1216878ec5bfed99fa50259811fb7e279af60
Milad Rahimi <info@miladrahimi.com>
2022-10-22 15:31:07 +0330

    update

--- v2ray-bridge-server/vmess.sh
+++ v2ray-bridge-server/vmess.sh
@@ -5,10 +5,14 @@ printf "BRIDGE-IP (Leave empty to use '%s'): " "$REAL_IP"
 read -r BRIDGE_IP
 BRIDGE_IP=${BRIDGE_IP:-$REAL_IP}
 
+printf "BRIDGE-PORT (Leave empty to use '%s'): " "1310"
+read -r BRIDGE_PORT
+BRIDGE_PORT=${BRIDGE_PORT:-1310}
+
 printf "BRIDGE-UUID: "
 read -r BRIDGE_UUID
 
-JSON="{\"add\": \"$BRIDGE_IP\", \"aid\": \"0\", \"host\": \"\", \"id\": \"$BRIDGE_UUID\", \"net\": \"tcp\", \
-\"path\": \"\", \"port\": \"1310\", \"ps\": \"$BRIDGE_IP/tcp\", \"tls\": \"\", \"type\": \"none\", \"v\": \"2\"}"
+JSON="{\"add\": \"$BRIDGE_IP\", \"aid\": \"0\", \"host\": \"\", \"id\": \"$BRIDGE_UUID\", \"net\": \"tcp\", \"path\": \
+\"\", \"port\": \"$BRIDGE_PORT\", \"ps\": \"$BRIDGE_IP/tcp\", \"tls\": \"\", \"type\": \"none\", \"v\": \"2\"}"
 
 printf "\n%s:\n%s%s\n" "VMESS Link" "vmess://" "$(echo "$JSON" | base64)"
