8d4c4d7c3d10143dad57efe0ea29dfcc42a2f413
Milad Rahimi <info@miladrahimi.com>
2022-11-08 16:12:03 +0330

    update

--- v2ray-bridge-server/clients.py
+++ v2ray-bridge-server/clients.py
@@ -10,8 +10,6 @@ path = Path(__file__).parent
 file = open(str(path.joinpath('config/config.json')), 'r', encoding='utf-8')
 config = json.load(file)
 
-html = open(str(path.joinpath('web/index.html')), 'r', encoding="utf-8").read()
-
 ip = urlopen("http://ifconfig.io/ip").read().decode().rstrip()
 
 for inbound in config['inbounds']:
@@ -26,7 +24,6 @@ for inbound in config['inbounds']:
         security = base64.b64encode((method + ":" + password).encode('ascii')).decode('ascii')
         link = "ss://{}@{}:{}#{}:{}".format(security, ip, port, ip, port)
         print("\nShadowsocks:\n" + link)
-        html = re.sub(r'(ss://[^<]+)', link, html)
     if inbound['protocol'] == 'vmess':
         port = str(inbound['port'])
         uuid = inbound['settings']['clients'][0]['id']
@@ -37,6 +34,3 @@ for inbound in config['inbounds']:
         j = json.dumps(c)
         link = "vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii')
         print("\nVMESS:\n" + link)
-        html = re.sub(r'(vmess://[^<]+)', link, html)
-
-open(str(path.joinpath('web/index.html')), 'w', encoding='utf-8').write(html)
