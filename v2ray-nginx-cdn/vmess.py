c27e08b40254984facd0d9ec35c53338bd667ff1
ohmydevops <ohmydevops@gmail.com>
2022-12-16 11:44:20 +0330

    Fix nginx script

--- v2ray-nginx-cdn/vmess.py
+++ v2ray-nginx-cdn/vmess.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import base64
+import yaml
 import json
 from pathlib import Path
 
 path = Path(__file__).parent
 
-config_file = open(str(path.joinpath('v2ray/config/config.json')), 'r', encoding='utf-8')
-config = json.load(config_file)
+v2ray_config_file = open(str(path.joinpath('v2ray/config/config.json')), 'r', encoding='utf-8')
+v2ray_config = json.load(v2ray_config_file)
+with open('docker-compose.yml', 'r') as f:
+    dockerCompose = yaml.safe_load(f)
 
-caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read()
-
-uuid = config['inbounds'][0]['settings']['clients'][0]['id']
-domain = caddy[:caddy.find(' {')]
+uuid = v2ray_config['inbounds'][0]['settings']['clients'][0]['id']
+domain = dockerCompose["services"]["v2ray"]["environment"][1].split('=')[1];
 
 j = json.dumps({
     "v": "2", "ps": domain, "add": domain, "port": "443", "id": uuid, "aid": "0", "net": "ws", "type": "none",
