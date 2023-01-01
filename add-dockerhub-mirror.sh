d9bb2df677b335c49ba1e2082c48ea97d9fa343d
Amirhossein Baghaie <ohmydevops@gmail.com>
2023-01-01 21:30:18 +0330

    Update add-dockerhub-mirror.sh

--- add-dockerhub-mirror.sh
+++ add-dockerhub-mirror.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Docs: https://mirror.iranserver.com/docker/
 
@@ -7,13 +8,15 @@ echo -e "Updating your Docker registry to docker.iranserver.com mrirror ...\n"
 echo -e "Backuping your current /etc/docker/daemon.json file into the /etc/docker/daemon.json.old ...\n"
 sudo cp /etc/docker/daemon.json /etc/docker/daemon.json.old
 
-sudo cat <<EOT > /etc/docker/daemon.json
+
+sudo bash -c "cat <<EOT > /etc/docker/daemon.json
 {
-  "registry-mirrors": ["https://docker.iranserver.com"]
+  "registry-mirrors": [\"https://docker.iranserver.com\"]
 }
-EOT
+EOT"
 
 echo -e "Restarting your docker daemon ...\n"
+sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 echo -e "Done! for test: 'docker run hello-world'\n"
