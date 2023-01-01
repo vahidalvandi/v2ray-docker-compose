3a8e51d8b073f53d2aa6465ad8dfbc7922b42d56
Amirhossein Baghaie <ohmydevops@gmail.com>
2023-01-01 21:36:40 +0330

    Add check for /etc/docker/daemon.json

--- add-dockerhub-mirror.sh
+++ add-dockerhub-mirror.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 
+DOCKER_DAEMON_FILE="/etc/docker/daemon.json"
+
 # Docs: https://mirror.iranserver.com/docker/
 
 echo -e "Updating your Docker registry to docker.iranserver.com mrirror ...\n"
+
+if [ ! -f "$DOCKER_DAEMON_FILE" ]; then
+  echo -e "Creating your docker daemon config file ...\n"
+  touch /etc/docker/daemon.json
+fi
 
 echo -e "Backuping your current /etc/docker/daemon.json file into the /etc/docker/daemon.json.old ...\n"
 sudo cp /etc/docker/daemon.json /etc/docker/daemon.json.old
 
 
-sudo bash -c "cat <<EOT > /etc/docker/daemon.json
+sudo bash -c "cat <<EOT > $DOCKER_DAEMON_FILE
 {
   "registry-mirrors": [\"https://docker.iranserver.com\"]
 }
